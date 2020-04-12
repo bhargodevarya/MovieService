a8283e5bf08301ece7a6ee58ea3975512b5e290a
Bhargodev Arya <bhargodevarya@gmail.com>
2020-04-13 00:08:28 +0530

    test deployment from travis

--- MovieServiceApp/src/main/java/com/app/api/Movie.java
+++ MovieServiceApp/src/main/java/com/app/api/Movie.java
@@ -33,6 +33,6 @@ public class Movie implements MovieApi {
     @Override
     @GetMapping(value = "/health")
     public String health() {
-        return "healthy deployment auto deploy";
+        return "healthy deployment auto deploy test from travis";
     }
 }
